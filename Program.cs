49548c050a8ad49ddc1058f94b895c2a2e9c7a85
Luis <m.luishernandez@outlook.com>
2020-09-22 02:40:04 -0400

    added switch statements

--- Program.cs
+++ Program.cs
@@ -6,7 +6,84 @@ namespace Hotel_Console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+        Console.WriteLine("Welcome to the Carlington Hotel");
+        Console.WriteLine("Which kind of room would you like?");
+        Console.WriteLine("Press 1 for single, 2 for double and 3 for suite");
+        var room = Int32.Parse(Console.ReadLine());
+
+        switch (room)
+        {
+            case 1:
+            Console.WriteLine("You selected single");
+            Console.WriteLine("What floor do you wish to stay?");
+            var floor = Int32.Parse(Console.ReadLine());
+
+            if (floor <= 5)
+            {
+                Console.WriteLine("You have chosen floor number " + floor);
+                Console.WriteLine("The cost per night is $45.00");
+                Console.WriteLine("How long will you stay?");
+                var stay = Int32.Parse(Console.ReadLine());
+                
+                
+                if (stay <= 3)
+                {
+                    Console.WriteLine("You've chosen "+ stay + " nights");
+                    var cost = 45.00;
+                    var total = stay * cost;
+                    Console.WriteLine("Your total is $"+total); 
+                    
+                   
+                }
+            }
+            break;
+
+            case 2:
+
+            Console.WriteLine("You selected double");
+            Console.WriteLine("What floor do you wish to stay?");
+            var floornum2 = Int32.Parse(Console.ReadLine());
+
+            if (floornum2 <= 5)
+            {
+                Console.WriteLine("You have chosen floor number " + floornum2);
+                Console.WriteLine("How long will you stay?");
+                var stay = Int32.Parse(Console.ReadLine());
+                
+                if (stay <= 3)
+                {
+                    Console.WriteLine("You've chosen "+ stay + " nights");
+                }
+            }
+            break;
+
+            case 3:
+
+            Console.WriteLine("You selected suite");
+            Console.WriteLine("What floor do you wish to stay?");
+            var floornum3 = Int32.Parse(Console.ReadLine());
+
+            if (floornum3 <= 5)
+            {
+                Console.WriteLine("You have chosen floor number " + floornum3);
+                Console.WriteLine("How long will you stay?");
+                var stay = Int32.Parse(Console.ReadLine());
+                
+                if (stay <= 3)
+                {
+                    Console.WriteLine("You've chosen "+ stay + " nights");
+                }
+            }
+            break;
+
+            
+
+        }
+
+
+
+
         }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Hotel_Console
+
+The Carlington Hotel wants  a program in which the user enters the type of room that a guest wants (1 for single, 2 for double, 3 for suite), the floor desired (1 through 12), and the number of nights the guest will stay (any number is valid). Compute the nightly rate from the following table, multiply by the number of nights, and then provide a 10% discount on the total if the guest stays 4 or more nights.
+
+                Single      Double      Suite
+
+ Floor 1-5      45.00       60.00       130.00
+ Floor 6-11     55.00       72.00       215.00
+ Floor 12   Not Available  120.00       350.00
+
+ Display the input data, the nightly price, and the total price for the guests stay, or display an error message if a combination is not available or if an invaild room type code or floor is entered.
